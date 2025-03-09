30eb4365c7fa51addaf2a2c2e4993d7d06415747
Mohammed Adib <mohammed.adib1104@gmail.com>
2025-03-09 19:04:20 +0600

    math added

---
+++ math.js
@@ -0,0 +1,23 @@
+console.log(Math.PI);
+
+let x = 2.12;
+
+// console.log(x);
+// console.log(Math.round(x));
+// console.log(Math.floor(x));
+
+let num1 = 12;
+let num2 = 23;
+
+// console.log(Math.max(num1, num2));
+// random
+
+let randomNumber = Math.floor(Math.random() * 6);
+console.log(randomNumber);
+
+// using min and max value
+const min = 50;
+const max = 100;
+console.log(
+    Math.floor(Math.random() * max) + min
+);
